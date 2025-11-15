deeeffd033c8b5c09fb4e9c6a69a38433577bcda
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-11-16 00:13:53 +0200

    Implement UseCase base class and refactor GetFeaturedBooksUseCase and GetNewestBooksUseCase to extend it

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -2,13 +2,15 @@ import 'package:bookly/core/errors/failures.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:bookly/core/use_case/use_case.dart';
 
-class GetFeaturedBooksUseCase {
+class GetFeaturedBooksUseCase extends UseCase<List<BookEntity>, void> {
   final HomeRepo homeRepo;
 
   GetFeaturedBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> call() async {
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void params]) async {
     return await homeRepo.fetchFeaturedBooks();
   }
 }
--- lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,14 +1,16 @@
 import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/use_case/use_case.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class GetNewestBooksUseCase {
+class GetNewestBooksUseCase extends UseCase<List<BookEntity>, void> {
   final HomeRepo homeRepo;
 
   GetNewestBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> getNewestBooksUseCase() async {
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void params]) async {
     return await homeRepo.fetchNewestBooks();
   }
 }
